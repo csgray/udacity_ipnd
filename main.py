d6786fd8806b4cdb97eb1433fb1c49f4488c5fa2
csgray <csgray@alaska.edu>
2016-02-05 12:44:45 -0900

    Fix CSS

--- main.py
+++ main.py
@@ -17,14 +17,25 @@ lesson_notes = """
     </section>
 """
 
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
+
 class Handler(webapp2.RequestHandler):
     def write(self, *args, **kwargs):
-        self.response.headers['Content-Type'] = 'html'
         self.response.out.write(*args, **kwargs)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kwargs):
+        self.write(self.render_str(template, **kwargs))
 
 class MainPage(Handler):
     def get(self):
-        output = lesson_notes
-        self.write(output)
+#       output = lesson_notes
+#       self.write(output)
+        self.render("lesson_notes.html")
 
 app = webapp2.WSGIApplication([("/", MainPage)], debug=True)
