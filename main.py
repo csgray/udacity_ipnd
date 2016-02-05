5af5008aadd59b73cfdb9d6123b76e1db8f05c2d
csgray <csgray@alaska.edu>
2016-02-05 09:08:38 -0900

    Working simple GAE

--- main.py
+++ main.py
@@ -2,10 +2,29 @@ import os
 import jinja2
 import webapp2
 
+lesson_notes = """
+    <section class="section">
+      <h2 class="subheading">The Internet</h2>
+        <p class="text">The internet is a network of computers communicating
+         with each other through the use of HTTP requests. Client computers
+         request <em>HTML</em> documents from web servers &lpar;any computer
+         hosting a file can be a server though "servers" usually refer to
+         dedicated platforms&rpar; through the internet. The server sends the
+         HTML file to the client computer where a browser interprets and
+         displays the file. </p>
+        <p class="text">Most Alaskans know that the internet is not a series
+         of tubes.</p>
+    </section>
+"""
 
-class MainPage(webapp2.RequestHandler):
+class Handler(webapp2.RequestHandler):
+    def write(self, *args, **kwargs):
+        self.response.headers['Content-Type'] = 'html'
+        self.response.out.write(*args, **kwargs)
+
+class MainPage(Handler):
     def get(self):
-        os.path = os.path.join(os.path.dirname(__file__), 'lesson_notes.html')
-        self.response.out.write()
+        output = lesson_notes
+        self.write(output)
 
-app = webapp2.WSGIApplication([('/', MainPage),], debug=True)
+app = webapp2.WSGIApplication([("/", MainPage)], debug=True)
